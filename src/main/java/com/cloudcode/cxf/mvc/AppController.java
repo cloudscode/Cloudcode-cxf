896ff1c82f1b9ee2b35c052f86cee77afedad40f
via <ljzhuanjiao@gmail.com>
2016-08-17 13:50:21 +0800

    Signed-off-by: via <ljzhuanjiao@gmail.com>

---
+++ src/main/java/com/cloudcode/cxf/mvc/AppController.java
@@ -0,0 +1,29 @@
+package com.cloudcode.cxf.mvc;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.cloudcode.cxf.dao.AppDao;
+import com.cloudcode.cxf.model.App;
+import com.cloudcode.framework.controller.CrudController;
+
+@Controller
+@RequestMapping("/app")
+public class AppController extends CrudController<App> {
+	@Autowired
+	private AppDao appDao;
+
+	@RequestMapping(value = "/test", method = { RequestMethod.POST,
+			RequestMethod.GET })
+	public @ResponseBody void test(@ModelAttribute  @Valid App app, HttpServletRequest request) {
+		appDao.loadAppAll();
+	}
+	
+}
